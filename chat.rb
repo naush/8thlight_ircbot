379f7a31504d67b5016245dbfdf3b7741e3857df
naush <lihsuan@8thlight.com>
2012-10-29 18:02:49 -0500

    Chat locally

---
+++ chat.rb
@@ -0,0 +1,13 @@
+require_relative 'lib/irc/ai/markov'
+
+ai = IRC::AI::Markov.new
+
+while true
+  print "< "
+  user_input = $stdin.gets
+  break if ["quit\n", "q\n"].include?(user_input.downcase)
+  puts "> " + ai.read(user_input)
+  ai.write(user_input)
+end
+
+puts "> See ya!"
