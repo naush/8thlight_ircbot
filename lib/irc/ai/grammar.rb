02ec27a4d4d2148c2c37de1331820e28dae1230d
naush <lihsuan@8thlight.com>
2012-11-11 09:08:42 -0600

    Convert question words to file

--- lib/irc/ai/grammar.rb
+++ lib/irc/ai/grammar.rb
@@ -1,39 +1,13 @@
 module IRC
   module AI
     module Grammar
-      QUESTION_WORDS = [
-        'what',
-        'when',
-        'where',
-        'which',
-        'who',
-        'whom',
-        'whose',
-        'why',
-        'how',
-        'should',
-        'shouldn\'t',
-        'can',
-        'can\'t',
-        'do',
-        'don\'t',
-        'does',
-        'doesn\'t',
-        'have',
-        'haven\'t',
-        'am',
-        'are',
-        'aren\'t',
-        'is',
-        'isn\'t'
-      ]
-
       def self.format(sentence)
         sentence[0].capitalize + sentence[1..-1] + stop(sentence.split.first)
       end
 
       def self.stop(first_word)
-        return '?' if QUESTION_WORDS.include?(first_word)
+        @question_words ||= IO.read(File.dirname(__FILE__) + '/corpus/_question_words')
+        return '?' if @question_words.include?(first_word)
         return '.'
       end
     end
--- spec/ai/grammar_spec.rb
+++ spec/ai/grammar_spec.rb
@@ -2,6 +2,10 @@ require_relative '../spec_helper'
 require_relative '../../lib/irc/ai/grammar'
 
 describe IRC::AI::Grammar do
+  before do
+    IO.stub(:read).and_return(['what'])
+  end
+
   it "capitalizes a sentence" do
     IRC::AI::Grammar.format("he is a robot").should == "He is a robot."
   end
@@ -10,32 +14,7 @@ describe IRC::AI::Grammar do
     IRC::AI::Grammar.format("he has a heart").should == "He has a heart."
   end
 
-  it "adds a question mark  sentence if starts with a question word" do
+  it "adds a question mark to sentence if starts with a question word" do
     IRC::AI::Grammar.format("what").should == "What?"
-    IRC::AI::Grammar.format("when").should == "When?"
-    IRC::AI::Grammar.format("where").should == "Where?"
-    IRC::AI::Grammar.format("which").should == "Which?"
-    IRC::AI::Grammar.format("who").should == "Who?"
-    IRC::AI::Grammar.format("whom").should == "Whom?"
-    IRC::AI::Grammar.format("whose").should == "Whose?"
-    IRC::AI::Grammar.format("why").should == "Why?"
-    IRC::AI::Grammar.format("how").should == "How?"
-
-    IRC::AI::Grammar.format("should").should == "Should?"
-    IRC::AI::Grammar.format("shouldn't").should == "Shouldn't?"
-    IRC::AI::Grammar.format("can").should == "Can?"
-    IRC::AI::Grammar.format("can't").should == "Can't?"
-    IRC::AI::Grammar.format("do").should == "Do?"
-    IRC::AI::Grammar.format("don't").should == "Don't?"
-    IRC::AI::Grammar.format("does").should == "Does?"
-    IRC::AI::Grammar.format("doesn't").should == "Doesn't?"
-    IRC::AI::Grammar.format("have").should == "Have?"
-    IRC::AI::Grammar.format("haven't").should == "Haven't?"
-
-    IRC::AI::Grammar.format("am").should == "Am?"
-    IRC::AI::Grammar.format("are").should == "Are?"
-    IRC::AI::Grammar.format("aren't").should == "Aren't?"
-    IRC::AI::Grammar.format("is").should == "Is?"
-    IRC::AI::Grammar.format("isn't").should == "Isn't?"
   end
 end
